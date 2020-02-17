62686d805e1ed2156cb8eee4dc128a83164f3393
Shakil Mahmud <shakil@bluetechbd.com>
2020-02-17 12:23:35 +0600

    product controller added + api added

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+// use Illuminate\Routing\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('/product','ProductController');
+
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
+
+?>
