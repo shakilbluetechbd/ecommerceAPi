13eb54b1cdb8c04bbecdb9d1ce7ff51db4a8a0ba
Shakil Mahmud <shakil@bluetechbd.com>
2020-02-17 16:05:08 +0600

    relationship created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,7 +4,13 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Product;
+
 class Review extends Model
 {
     //
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
