d85675b19a36f15850e79c9c1d60dc9aa7d12a17
Shakil Mahmud <shakil@bluetechbd.com>
2020-02-18 15:54:56 +0600

    transforming product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,11 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name'=>$this->name,
-            'description'=>$this->detail,
-            'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'totalPrice'=>$this->price*(1- $this->discount/100),
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/product','ProductController');
+Route::apiResource('/products','ProductController');
 
 Route::group(['prefix'=>'products'],function(){
     Route::apiResource('/{product}/reviews','ReviewController');
